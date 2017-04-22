4b1d8082ff256603ceffe5df2a5e4c944d58f583
LolloK5 <lorenzobettin25@gmail.com>
2017-04-22 09:12:29 +0200

    Add files via upload

---
+++ Unsigned.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    unsigned int max=4294967295
+    unsigned int min=0
+    cout<<"il minimo:"<<min<endl;
+    cout<<"il massimo:"<<max<endl;
+    cout<<"il minimo -1:"<<min-1<<endl;
+    cout<<"il massimo +1:"<<max+1<<endl;
+    retourn 0;
+}
